8ffaf9d101d364da17c9f09597e5267a5837c0bd
Navarone <Navarone@DESKTOP-O9INSPU>
2025-05-06 16:07:18 +0800

    Add project files.

---
+++ DILG/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DILG.Controllers
+{
+    public class AdminController : Controller
+    {
+        private DILG_DBEntities db = new DILG_DBEntities();
+        // GET: Admin
+        public ActionResult AdminLayout()
+        {
+            return View(); // This should return the view associated with AdminLayout
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult UserManager()
+        {
+            return View(db.Employees.ToList());
+        }
+
+
+
+
+
+        public ActionResult PositionManager()
+        {
+            return View();
+        }
+        public ActionResult OfficeManager()
+        {
+            return View();
+        }
+
+        public ActionResult ReportHistory() 
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ DILG/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DILG.Views.User
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SubmitReport()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            var viewModel = new DatatablesViewModel();
+
+            if (file != null && file.ContentLength > 0)
+            {
+                // Validate the file type
+                if (!viewModel.IsValidFileType(file.FileName))
+                {
+                    ModelState.AddModelError("File", "Invalid file type. Please upload a PDF, Word, Excel, JPG, or PNG file.");
+                    return View(); // Return to the view with the error
+                }
+
+                // Process the file (e.g., save it, etc.)
+                // ...
+            }
+
+            return View();
+        }
+    }
+}
